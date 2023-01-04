b8b43f361bbd1930fb256a60430f0fab168063b5
Pete Dring <dringp@fulford.york.sch.uk>
2023-01-04 18:38:53 +0000

    Simple UI

--- lib.js
+++ lib.js
@@ -1,7 +1,28 @@
 var metronome = {
-    init: () => {
+    bpm: 60,
 
-    }
+    init: () => {
+        metronome.elements = {
+            bpm: document.getElementById("bpm"),
+            slower: document.getElementById("btn-slower"),
+            faster: document.getElementById("btn-faster")
+        };
+
+        metronome.elements.slower.onclick = (e) => {
+            metronome.updateBPM(metronome.bpm - 5);
+        }
+
+        metronome.elements.faster.onclick = (e) => {
+            metronome.updateBPM(metronome.bpm + 5);
+        }
+    },
+
+    updateBPM: (bpm) => {
+        metronome.bpm = bpm;
+        metronome.elements.bpm.value = bpm;
+    },
+
+    playing: false
 }
 
 metronome.init();
