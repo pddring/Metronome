76a26307011da59235fdc9f40f49ef42f71dff48
fulfordcs <fulfordcs@gmail.com>
2023-01-16 11:23:25 +0000

    Today's lesson

--- lib.js
+++ lib.js
@@ -3,8 +3,31 @@ console.log("Setting up metronome");
 const btnDecreaseBPM = document.getElementById("btnDecreaseBPM");
 const btnIncreaseBPM = document.getElementById("btnIncreaseBPM");
 const bpmInput = document.getElementById("txtBPM");
-
+const btnStartStop = document.getElementById("btnStartStop");
+const MAX_BPM = 120;
+const MIN_BPM = 50;
+var running = false;
 var bpm = 60;       // beats per minute
+var callback = undefined;
+
+btnStartStop.addEventListener("click", function (e) {
+    running = !running;
+    btnStartStop.value = running ? "Stop" : "Start";
+
+    if (running) {
+        setSpeedAndStart();
+    }
+});
+
+function setSpeedAndStart() {
+    clearInterval(callback);
+    var ms = 60000 / bpm;
+    callback = setInterval(onTick, ms);
+}
+
+function onTick() {
+    console.log("Tick");
+}
 
 // add event handlers
 bpmInput.onchange = function (e) {
@@ -17,4 +40,28 @@ bpmInput.onchange = function (e) {
 
 btnDecreaseBPM.onclick = function (e) {
     bpm -= 5;
+    updateBPM();
+}
+
+btnIncreaseBPM.onclick = function (e) {
+    bpm += 5;
+    updateBPM();
+}
+
+
+
+function updateBPM() {
+    // range check
+    if (bpm > MAX_BPM) {
+        bpm = MAX_BPM;
+    }
+
+    if (bpm < MIN_BPM) {
+        bpm = MIN_BPM;
+    }
+
+    bpmInput.value = bpm;
+    if (running) {
+        setSpeedAndStart();
+    }
 }
